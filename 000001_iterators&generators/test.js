8767c5a0c2be042488bea33dfbcb72cfdad1e8e3
Anandhan <anandhanedom@gmail.com>
2020-11-25 23:39:23 +0530

    cls

--- 000001_iterators&generators/test.js
+++ 000001_iterators&generators/test.js
@@ -1,17 +1,26 @@
-let view;
+// let view;
 
-function initialize() {
-  let i = 0;
-  return () => {
-    if (i > 0) {
-      return;
-    } else {
-      view = 'Hills';
-      i++;
-      console.log('view has been set!');
-    }
-  };
+// function initialize() {
+//   let i = 0;
+//   return () => {
+//     if (i > 0) {
+//       return;
+//     } else {
+//       view = 'Hills';
+//       i++;
+//       console.log('view has been set!');
+//     }
+//   };
+// }
+
+// const callit = initialize();
+// callit();
+
+const array = [1, 2, 3, 4];
+
+const array = [1, 2, 3, 4];
+for (let i = 0; i < array.length; i++) {
+  setTimeout(function () {
+    console.log('I am at index ' + i);
+  }, 3000);
 }
-
-const callit = initialize();
-callit();
