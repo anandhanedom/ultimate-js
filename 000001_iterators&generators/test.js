181c2a6182cf8072da6cc009591e0f1f3c2e93ac
Anandhan <anandhanedom@gmail.com>
2020-11-26 15:26:43 +0530

    prototype

--- 000001_iterators&generators/test.js
+++ 000001_iterators&generators/test.js
@@ -16,11 +16,19 @@
 // const callit = initialize();
 // callit();
 
-const array = [1, 2, 3, 4];
+// const array = [1, 2, 3, 4];
 
-const array = [1, 2, 3, 4];
-for (let i = 0; i < array.length; i++) {
-  setTimeout(function () {
-    console.log('I am at index ' + i);
-  }, 3000);
-}
+// const array = [1, 2, 3, 4];
+// for (let i = 0; i < array.length; i++) {
+//   setTimeout(function () {
+//     console.log('I am at index ' + i);
+//   }, 3000);
+// }
+
+// Date.prototype.lastYear = function () {
+//   return this.getFullYear() - 1;
+// };
+
+// const prevYear = new Date('1900-10-10').lastYear();
+
+// console.log(prevYear);
