1b67dbc7344c1f987a583e13c900969a586da8b2
Anandhan <anandhanedom@gmail.com>
2020-11-27 11:26:53 +0530

    ss

--- 000002_OOP /index.js
+++ 000002_OOP /index.js
@@ -1,12 +1,18 @@
 //Factory functions
+
+const elfFunctions = {
+  attack() {
+    return 'attack with ' + this.weapon;
+  },
+};
+
 function createElf(name, weapon) {
-  return {
-    name,
-    weapon,
-    attack() {
-      return 'attack with ' + weapon;
-    },
-  };
+  const newElf = Object.create(elfFunctions);
+
+  newElf.name = name;
+  newElf.weapon = weapon;
+
+  return newElf;
 }
 
 const peter = createElf('Peter', 'stones');
