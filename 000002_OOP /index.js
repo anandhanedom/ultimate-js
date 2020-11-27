cc81f8cec94992108859b9258962b01c5ad8e1f3
Anandhan <anandhanedom@gmail.com>
2020-11-27 12:45:40 +0530

    this

--- 000002_OOP /index.js
+++ 000002_OOP /index.js
@@ -1,22 +1,59 @@
-//Factory functions
+// //Factory functions
+// const elfFunctions = {
+//   attack() {
+//     return 'attack with ' + this.weapon;
+//   },
+// };
 
-const elfFunctions = {
-  attack() {
-    return 'attack with ' + this.weapon;
-  },
-};
+// function createElf(name, weapon) {
+//   const newElf = Object.create(elfFunctions);
+
+//   newElf.name = name;
+//   newElf.weapon = weapon;
+
+//   return newElf;
+// }
+
+// const peter = createElf('Peter', 'stones');
+// const sam = createElf('Sam', 'fire');
+
+// console.log(peter.attack());
+// console.log(sam.attack());
+
+// // Array.prototype.map = function () {
+// //   let arr = [];
+// //   for (var i = 0; i < this.length; i++) {
+// //     arr.push(this[i] + '!');
+// //   }
+
+// //   return arr;
+// // };
+
+// // console.log([1, 2, 3].map());
 
 function createElf(name, weapon) {
-  const newElf = Object.create(elfFunctions);
-
-  newElf.name = name;
-  newElf.weapon = weapon;
-
-  return newElf;
+  this.name = name;
+  this.weapon = weapon;
 }
 
-const peter = createElf('Peter', 'stones');
-const sam = createElf('Sam', 'fire');
+const peter = new createElf('Peter', 'stones');
+const sam = new createElf('Sam', 'fire');
 
-console.log(peter.attack());
-console.log(sam.attack());
+createElf.prototype.attack = function () {
+  return 'attack with ' + this.weapon;
+};
+
+createElf.prototype.build = function () {
+  building = () => {
+    console.log(this);
+    return this.name + ' builds a home';
+  };
+
+  return building;
+};
+
+// console.log(peter.attack());
+// console.log(sam.attack());
+
+console.log(sam.build()());
+console.log(peter.build()());
