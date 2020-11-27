67780618838be1372072c3074639fdb6cffcbb7d
Anandhan <anandhanedom@gmail.com>
2020-11-27 12:55:32 +0530

    scoping

--- 000002_OOP /index.js
+++ 000002_OOP /index.js
@@ -1,59 +1,82 @@
-// //Factory functions
-// const elfFunctions = {
-//   attack() {
-//     return 'attack with ' + this.weapon;
+// // //Factory functions
+// // const elfFunctions = {
+// //   attack() {
+// //     return 'attack with ' + this.weapon;
+// //   },
+// // };
+
+// // function createElf(name, weapon) {
+// //   const newElf = Object.create(elfFunctions);
+
+// //   newElf.name = name;
+// //   newElf.weapon = weapon;
+
+// //   return newElf;
+// // }
+
+// // const peter = createElf('Peter', 'stones');
+// // const sam = createElf('Sam', 'fire');
+
+// // console.log(peter.attack());
+// // console.log(sam.attack());
+
+// // // Array.prototype.map = function () {
+// // //   let arr = [];
+// // //   for (var i = 0; i < this.length; i++) {
+// // //     arr.push(this[i] + '!');
+// // //   }
+
+// // //   return arr;
+// // // };
+
+// // // console.log([1, 2, 3].map());
+
+// function createElf(name, weapon) {
+//   this.name = name;
+//   this.weapon = weapon;
+// }
+
+// const peter = new createElf('Peter', 'stones');
+// const sam = new createElf('Sam', 'fire');
+
+// createElf.prototype.attack = function () {
+//   return 'attack with ' + this.weapon;
+// };
+
+// createElf.prototype.build = function () {
+//   building = () => {
+//     console.log(this);
+//     return this.name + ' builds a home';
+//   };
+
+//   return building;
+// };
+
+// // console.log(peter.attack());
+// // console.log(sam.attack());
+
+// console.log(sam.build()());
+// console.log(peter.build()());
+
+// const obj = {
+//   name: 'Hitler',
+//   speak() {
+//     return () => {
+//       console.log(this);
+//       return `My name is ${this.name}`;
+//     };
 //   },
 // };
 
-// function createElf(name, weapon) {
-//   const newElf = Object.create(elfFunctions);
+// const sp = obj.speak();
 
-//   newElf.name = name;
-//   newElf.weapon = weapon;
+// console.log(sp());
 
-//   return newElf;
+// let i = 1;
+
+// if (true) {
+//   let i = 2;
+//   console.log('Inner i:', i);
 // }
 
-// const peter = createElf('Peter', 'stones');
-// const sam = createElf('Sam', 'fire');
-
-// console.log(peter.attack());
-// console.log(sam.attack());
-
-// // Array.prototype.map = function () {
-// //   let arr = [];
-// //   for (var i = 0; i < this.length; i++) {
-// //     arr.push(this[i] + '!');
-// //   }
-
-// //   return arr;
-// // };
-
-// // console.log([1, 2, 3].map());
-
-function createElf(name, weapon) {
-  this.name = name;
-  this.weapon = weapon;
-}
-
-const peter = new createElf('Peter', 'stones');
-const sam = new createElf('Sam', 'fire');
-
-createElf.prototype.attack = function () {
-  return 'attack with ' + this.weapon;
-};
-
-createElf.prototype.build = function () {
-  building = () => {
-    console.log(this);
-    return this.name + ' builds a home';
-  };
-
-  return building;
-};
-
-// console.log(peter.attack());
-// console.log(sam.attack());
-
-console.log(sam.build()());
-console.log(peter.build()());
+// console.log(('Outer i', i));
