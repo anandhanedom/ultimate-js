be21efb005adc0fbfdb15a9172148a14ab16884b
Anandhan <anandhanedom@gmail.com>
2020-11-29 01:52:18 +0530

    notes

---
+++ 000003_FP/ex10.js
@@ -0,0 +1,12 @@
+//Arity - no. of arguments
+//Try to stick to 1 or 2 parameters
+
+
+//Functional programming
+// 1. 1 task
+// 2. return statement
+// 3. Pure
+// 4. No shared state
+// 5. Immutable state
+// 6. Composable
+// 7. Predictable
