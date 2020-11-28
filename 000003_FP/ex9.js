17438f636252fd47e79e940a113bdb011588d24f
Anandhan <anandhanedom@gmail.com>
2020-11-29 01:33:20 +0530

    compose and pipe

---
+++ 000003_FP/ex9.js
@@ -0,0 +1,16 @@
+//Compose
+const makePositive = (n) => Math.abs(n);
+const mulThree = (n) => n * 3;
+
+const compose = (f, g) => (n) => f(g(n));
+
+const mulAndAbsThree = compose(mulThree, makePositive);
+
+console.log(mulAndAbsThree(-100));
+
+//Pipe
+const compose = (f, g) => (n) => g(f(n));
+
+// fn1(fn2(fn3(50)))
+// compose(fn1, fn2, fn3)(50);
+// pipe(fn3, fn2, fn1)(50);
