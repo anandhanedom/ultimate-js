851acb50977815c27e67598ec6b9769e97e4dc58
Anandhan <anandhanedom@gmail.com>
2020-11-29 23:32:17 +0530

    amazon

--- 000003_FP/exerciseAdvanced.js
+++ 000003_FP/exerciseAdvanced.js
@@ -1,11 +1,3 @@
-const user = {
-  name: 'Kim',
-  active: true,
-  cart: [],
-  purchases: [],
-  history: [],
-};
-
 // Implement  a cart feature:
 //     1. Add items to cart.
 //     2. Add 3% tax to item in cart
@@ -16,36 +8,77 @@ const user = {
 //  1. Accept refunds
 //  2. Track user history
 
-//Compose
-const compose = (f, g) => (...args) => f(g(...args));
+const user = {
+  name: 'Kim',
+  active: true,
+  cart: [],
+  purchases: [],
+};
 
-//Call
+const history = [];
+
+const compose = (f, g) => (...args) => f(g(...args));
+const pipe = (f, g) => (...args) => g(f(...args));
+
+const purchaseItem = (...fns) => fns.reduce(compose);
+const purchaseItem2 = (...fns) => fns.reduce(pipe);
+
+// purchaseItem2(
+//   addItemToCart,
+//   applyTaxToItems,
+//   buyItem,
+//   emptyUserCart
+// )(user, { name: 'laptop', price: 60 });
+
 console.log(
   purchaseItem(
-    emptyCart,
+    emptyUserCart,
     buyItem,
-    applyTax,
+    applyTaxToItems,
     addItemToCart
-  )(user, { name: 'Laptop', price: 200 })
+  )(user, { name: 'laptop', price: 50 })
 );
 
-//Functions
-function purchaseItem(...fns) {
-  return fns.reduce(compose);
-}
-
 function addItemToCart(user, item) {
+  history.push(user);
   const updatedCart = [...user.cart, item];
-
+  // return Object.assign({}, user, { cart: updatedCart });
   return { ...user, cart: updatedCart };
 }
 
-function applyTax(user) {
-  return user;
+function applyTaxToItems(user) {
+  history.push(user);
+  const { cart } = user;
+  const taxRate = 1.3;
+  const updatedCart = cart.map((item) => {
+    return {
+      name: item.name,
+      price: item.price * taxRate,
+    };
+  });
+  // return Object.assign({}, user, { cart: updatedCart });
+  return { ...user, cart: updatedCart };
 }
 
 function buyItem(user) {
-  return user;
+  history.push(user);
+  const itemsInCart = user.cart;
+  // return Object.assign({}, user, { purchases: itemsInCart });
+  return { ...user, purchases: itemsInCart };
 }
 
-function emptyCart() {}
+function emptyUserCart(user) {
+  history.push(user);
+  // return Object.assign({}, user, { cart: [] });
+  return { ...user, cart: [] };
+}
+
+console.log(history);
+
+function refundItem() {}
+
+function getUserState() {}
+
+function goBack() {}
+
+function goForward() {}
