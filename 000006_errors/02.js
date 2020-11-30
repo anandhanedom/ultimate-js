134e9040834462282c0bba6e37ca726031fc734d
Anandhan <anandhanedom@gmail.com>
2020-11-30 22:35:44 +0530

    s

--- 000006_errors/02.js
+++ 000006_errors/02.js
@@ -1,13 +1,31 @@
-class authError extends Error {
+class AuthError extends Error {
   constructor(msg) {
     super(msg);
 
-    this.name = 'authError';
+    this.name = 'AuthError';
     this.fav = 'Lemon';
   }
 }
 
-const a = new authError('Oops!');
+class DatabaseError extends Error {
+  constructor(msg) {
+    super(msg);
+
+    this.name = 'DatabaseError';
+    this.fav = 'Lemon';
+  }
+}
+
+class PermissionError extends Error {
+  constructor(msg) {
+    super(msg);
+
+    this.name = 'PermissionError';
+    this.fav = 'Lemon';
+  }
+}
+
+const a = new AuthError('Oops!');
 
 console.log(a.message);
 console.log(a.name);
